02e5edd41d94dbea9a3fa4171992997414667d9b
asosunoff <al.sosunoff@gmail.com>
2026-05-05 02:46:50 -0300

    checked config file

--- src/load-classes/get-content-classes-file.ts
+++ src/load-classes/get-content-classes-file.ts
@@ -6,14 +6,21 @@ import { CONFIG_NAME } from "../constants";
 
 export const getContentClassesFile = async () => {
   const configContent = await readConfig<{
-    classesFilePath?: [];
+    classesFilePath?: string[];
   }>(CONFIG_NAME);
 
-  if (!configContent) return;
+  if (!configContent || typeof configContent !== "object") return;
+
+  if (!("classesFilePath" in configContent)) {
+    vscode.window.showWarningMessage(
+      "CSS IntelliSense: config is missing classesFilePath",
+    );
+    return;
+  }
 
   if (!Array.isArray(configContent.classesFilePath)) {
     vscode.window.showWarningMessage(
-      `CSS IntelliSense: classesFilePath not an array`,
+      "CSS IntelliSense: classesFilePath must be an array",
     );
     return;
   }
